02056a5e7bec1453c952d363ce0c5d1198cac31b
Artem <xenex95@yandex.ru>
2023-01-30 01:38:53 +0500

    Создана программа для решения третьей задачи

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.WriteLine("Введите количество элементов массива:\t");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+double [] array = new double[elementsCount];
+
+Console.WriteLine("Заполните массив числами");
+
+void FillArray(double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t");
+        array[i] = Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+void Diff(double []array)
+{
+    double max = array [0];
+    double min = array [0];
+
+    for(int i=1;i<array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+    }
+    Console.WriteLine($"Максимальный элемент массива равен {max}");
+
+  
+    for(int i=1;i<array.Length; i++)
+    {
+        if(array[i] < min) min = array[i];
+    }
+    Console.WriteLine($"Минимальный элемент массива равен {min}");
+
+    double diff = max - min;
+    Console.WriteLine($"Разница равна {diff}");
+}
+
+FillArray(array);
+Diff(array);
